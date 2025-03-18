a251f9423e37411f4438614d996119d6c0861c6a
Alejandro Velasco <avelasco@conyko.com>
2025-03-18 09:53:11 -0500

    Inicialización del proyecto

---
+++ front/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    let count = $state(0);
+  </script>
+  
+  <div class="flex items-center justify-center h-screen bg-base-200">
+    <div class="hero bg-base-100 p-10 rounded-xl shadow-lg text-center">
+
+      <button class="btn btn-primary" onclick={() => count++}>
+        Count: {count}
+      </button>
+    </div>
+  </div>
+  
