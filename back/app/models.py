a6d1d68b63f8474489aebb175c972f290596a6b3
chuzasjota <chuzas.j@gmail.com>
2025-03-25 22:16:37 -0500

    Create ProspectUpdate model

--- back/app/models.py
+++ back/app/models.py
@@ -1,6 +1,8 @@
 # models.py
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from app.conection import Base
+from pydantic import BaseModel
+from typing import Optional
 
 class Prospect(Base):
     __tablename__ = 'prospects'
@@ -14,3 +16,15 @@ class Prospect(Base):
     country = Column(String)
     identification = Column(String, nullable=False, unique=True)
     status = Column(String, nullable=False)
+    deleted = Column(Boolean, default=False)  # Campo para soft delete
+
+# Modelo Pydantic para actualización parcial de un prospecto
+class ProspectUpdate(BaseModel):
+    email: Optional[str] = None
+    name: Optional[str] = None
+    phone: Optional[str] = None
+    city: Optional[str] = None
+    state: Optional[str] = None
+    country: Optional[str] = None
+    identification: Optional[str] = None
+    status: Optional[str] = None
